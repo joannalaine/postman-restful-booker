4c4aac10ed8e0d1bd5760593fd7ddeb4b760888c
Joanna Denni <joannanoriega@gmail.com>
2019-03-30 16:04:36 -0700

    docs(Response Content): fix comments to reflect updated parameters for Newman script

--- scripts/RespContLocal.sh
+++ scripts/RespContLocal.sh
@@ -5,9 +5,10 @@ REPORT_PATH="../reports/response-content"
 mkdir -p $REPORT_PATH
 
 # Using newman...
-# Run Smoke collection:     run ../collections/Smoke.postman_collection.json
+# Run ResponseContent collection:     run ../collections/ResponseContent.postman_collection.json
 #  in Local env:            -e ../env/Local.postman_environment.json
+#  load booking data file:  -d ../data/booking.json
 #  output via newman-reporter-htmlextra:    -r htmlextra
 #  export report to dir created above:      --reporter-htmlextra-export $REPORT_PATH
-#  with 'Smoke Test' title on HTML report page:     --reporter-htmlextra-title "Smoke Test"
+#  with 'Response Content Test' title on HTML report page:     --reporter-htmlextra-title "Response Content Test"
 newman run ../collections/ResponseContent.postman_collection.json -e ../env/Local.postman_environment.json -d ../data/booking.json -r htmlextra --reporter-htmlextra-export $REPORT_PATH --reporter-htmlextra-title "Response Content Test"
